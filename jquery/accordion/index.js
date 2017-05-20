e4e40d9bd799091468378dfb513a4a58c5c2dd99
Jyasveer Gotta <jgotta@redhat.com>
2017-05-20 12:21:42 +0530

    deleted the folders to check in later as a diff branch:wq

--- jquery/accordion/index.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    var $accordion = $('.accordion');
-    $accordion.find('.panel-heading').click(function(event){
-        $accordion.find('.panel-collapse').slideUp();
-        $(this).next().slideDown();
-    });
-});
